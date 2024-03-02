b79231062fabc389efe07417c44456859895e731
Mohamed Karroumi <mkaroumi123@gmail.com>
2024-03-02 04:46:40 +0000

    using webpack to bundle

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 
 export default class Renederer {
     private gl: CanvasRenderingContext2D;
@@ -10,8 +11,8 @@ export default class Renederer {
 
     public resizeCanvasToDisplaySize(): void {
         const canvas = this.gl.canvas as HTMLCanvasElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
+        const width = canvas.parentElement!.clientWidth;
+        const height = canvas.parentElement!.clientHeight;
         if (canvas.width !== width || canvas.height !== height) {
             canvas.width = width;
             canvas.height = height;
@@ -49,13 +50,21 @@ export default class Renederer {
         this.gl.strokeStyle = "transparent";
     }
 
-    public stroke(r?: number, g?: number, b?: number, a?: number): void {
+    public stroke1(r?: number, g?: number, b?: number, a?: number): void {
         if (r) {
             this.strokeColor = `rgba(${r},${g},${b},${a})`;
             this.gl.strokeStyle = this.strokeColor;
+            console.log(this.strokeColor);
+
         } else {
             this.gl.strokeStyle = this.strokeColor;
+            console.log(this.strokeColor);
+
         }
+    }
+    public stroke(r: number, g: number, b: number, a: number): void {
+        this.strokeColor = `rgba(${r},${g},${b},${a})`;
+        this.gl.strokeStyle = this.strokeColor;
     }
 
     public stroke2(r?: number | number[] | string, g?: number, b?: number, a?: number): void {
@@ -95,12 +104,34 @@ export default class Renederer {
         this.gl.closePath();
     }
 
+    public dashLine(x1: number, y1: number, x2: number, y2: number): void {
+        this.gl.beginPath();
+        this.gl.setLineDash([5, 15]);
+        this.gl.moveTo(x1, y1);
+        this.gl.lineTo(x2, y2);
+        this.gl.stroke();
+        this.gl.closePath();
+        this.gl.setLineDash([]);
+    }
+
     public rect(x: number, y: number, w: number, h: number): void {
         this.gl.beginPath();
         this.gl.rect(x, y, w, h);
         this.gl.fill();
         this.gl.stroke();
         this.gl.closePath();
+    }
+
+    public fillRect(x: number, y: number, w: number, h: number): void {
+        this.gl.fillRect(x, y, w, h);
+    }
+    public lineWidth(width: number): void {
+        this.gl.lineWidth = width;
+    }
+
+    public background(r: number, g: number, b: number, a: number): void {
+        this.gl.fillStyle = `rgba(${r},${g},${b},${a})`;
+        this.gl.fillRect(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     }
 
     public ellipse(x: number, y: number, w: number, h: number): void {
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,107 @@
 import Renederer from "./Renderer";
 
 let gl: CanvasRenderingContext2D;
+let mouseX = 0;
+let mouseY = 0;
 //empty div with class .mesh-grad
-const div = document.querySelector('.mesh-grad')!
+const div = document.querySelector(".mesh-grad")!;
 console.log(12);
 
-const canvas = document.createElement('canvas');
+const canvas = document.createElement("canvas");
 div.appendChild(canvas);
 canvas.width = div.clientWidth;
 canvas.height = div.clientHeight;
-gl = canvas.getContext('2d')!
+gl = canvas.getContext("2d")!;
+if (!gl) {
+    throw new Error("WebGL not supported");
+}
+
+canvas.addEventListener("touchstart", (e) => {
+    mouseX = e.touches[0].clientX;
+    mouseY = e.touches[0].clientY;
+});
+canvas.addEventListener("touchmove", (e) => {
+    mouseX = e.touches[0].clientX;
+    mouseY = e.touches[0].clientY;
+});
+canvas.addEventListener("mousedown", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+canvas.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
 
 const lerp = (start: number, end: number, t: number) => {
     return start * (1 - t) + end * t;
-}
+};
 
 const r = new Renederer(gl);
 
-r.resizeCanvasToDisplaySize();
-const draw = () => {
-    r.fill(255, 0, 0, 1);
-    r.noStroke();
-    r.rect(0, 0, 100, 100);
+div.addEventListener("resize", () => {
+    r.resizeCanvasToDisplaySize();
+});
+
+type Vec2 = { x: number, y: number };
+
+const drawLine = (start: Vec2, end: Vec2, a: number = 1) => {
+    r.noFill();
+    r.lineWidth(2);
+    r.stroke(0, 255, 0, a);
+    r.line(start.x, start.y, end.x, end.y);
+    r.fill(255, 0, 0, a);
+    r.circle(start.x, start.y, 5);
+    r.circle(end.x, end.y, 5);
 }
+
+const drawDashLine = (start: Vec2, end: Vec2, a: number = 1) => {
+    r.noFill();
+    r.lineWidth(1.5);
+    r.stroke(0, 167, 212, a);
+    r.dashLine(start.x, start.y, end.x, end.y);
+    r.fill(255, 120, 0, a);
+    r.circle(start.x, start.y, 5);
+    r.circle(end.x, end.y, 5);
+}
+
+const findMidPoint = (c: Vec2) => {
+    //nice color for the mid point
+    r.noFill();
+    r.lineWidth(1.5);
+    r.stroke(255, 0, 0, 1);
+    r.vertex(c.x, c.y);
+}
+const p0: Vec2 = { x: 100, y: 100 };
+let p1: Vec2 = { x: 300, y: 200 };
+const p2: Vec2 = { x: 100, y: 400 };
+
+function sine(t: number): number {
+    return Math.sin(t)*.5 + .5;
+}
+function lerpVec2(start: Vec2, end: Vec2, t: number): Vec2 {
+    return { x: lerp(start.x, end.x, t), y: lerp(start.y, end.y, t) };
+}
+let t = 0;
+let dt = 0.005;
+const draw = () => {
+    r.clear();
+    r.resizeCanvasToDisplaySize();
+    p1 = { x: mouseX, y: mouseY };
+    drawLine(p0, p1);
+    drawLine(p1, p2);
+    r.beginShape();
+    for (let i = 0; i < 1; i += .01) {
+        const p01 = lerpVec2(p0, p1, i);
+        const p12 = lerpVec2(p1, p2, i);
+        const p012 = lerpVec2(p01, p12, i);
+
+        findMidPoint(p012)
+    }
+
+    r.endShape();
+
+    requestAnimationFrame(draw);
+};
 
 draw();
